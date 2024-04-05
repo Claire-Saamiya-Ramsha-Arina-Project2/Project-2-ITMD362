48794099ca7f0c2f368bc4526d4746efe8e34018
ramsha0427 <rrizwan@hawk.iit.edu>
2024-04-05 05:41:46 -0500

    adding flashing text

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const span = document.getElementById('flashingText');
+
+    setInterval(() => {
+        span.style.visibility = (span.style.visibility === 'hidden' ? 'visible' : 'hidden');
+    }, 700);
+});
