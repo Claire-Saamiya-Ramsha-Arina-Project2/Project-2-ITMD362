535d234d3bbc2e09f987c7a44c3b65092bd6ae91
ramsha0427 <rrizwan@hawk.iit.edu>
2024-04-05 11:36:40 -0500

    changing blinking speed

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const span = document.getElementById('flashingText');
-
-    setInterval(() => {
-        span.style.visibility = (span.style.visibility === 'hidden' ? 'visible' : 'hidden');
-    }, 700);
-});
+var blink_speed = 1000; // every 1000 == 1 second, adjust to suit
+var t = setInterval(function () {
+    var ele = document.getElementById('flashingtext');
+    ele.style.visibility = (ele.style.visibility == 'hidden' ? '' : 'hidden');
+}, blink_speed);
