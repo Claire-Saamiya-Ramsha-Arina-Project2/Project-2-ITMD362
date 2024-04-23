2513bd194ddd414d8fef12e543e9c1380c54ea36
SaamiyaMaster <123525540+SaamiyaMaster@users.noreply.github.com>
2024-04-23 07:21:13 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,4 @@ var t = setInterval(function () {
     var ele = document.getElementById('flashingtext');
     ele.style.visibility = (ele.style.visibility == 'hidden' ? '' : 'hidden');
 }, blink_speed);
+
